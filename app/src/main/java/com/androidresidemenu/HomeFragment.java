008f053e5e9d076f4c590c29f5ab38d6cc53f7af
m2sdv <mona.basarkar@gmail.com>
2019-05-29 17:56:07 +0530

    This Is Reside Menu In Android

---
+++ app/src/main/java/com/androidresidemenu/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.androidresidemenu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.special.ResideMenu.ResideMenu;
+
+public class HomeFragment extends Fragment {
+
+    private View parentView;
+    private ResideMenu resideMenu;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        parentView = inflater.inflate(R.layout.home, container, false);
+        //setUpViews();
+        return parentView;
+    }
+}
